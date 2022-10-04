4e3215dbbfb538935c801fc838502e44fbceaccb
mj789-98 <manasjoshi2004.marvel.mj@gmail.com>
2022-10-04 15:11:52 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Recipe-List-App
+ A simple recipe list app made using flutter and  Yummly2 (rapid API)
---
+++ lib/models/recipe.api.dart
@@ -0,0 +1,26 @@
+import 'dart:convert';
+import 'package:flutter_application_2/models/recipe.dart';
+
+import 'package:http/http.dart' as http;
+
+class RecipeApi {
+  static Future<List<Recipe>> getRecipe() async {
+    var uri = Uri.https('yummly2.p.rapidapi.com', '/feeds/list',
+        {"limit": "18", "start": "0", "tag": "list.recipe.popular"});
+
+    final response = await http.get(uri, headers: {
+      "x-rapidapi-key": "987598e098msh692d64255090504p13a789jsn077f8f498e51",
+      "x-rapidapi-host": "yummly2.p.rapidapi.com",
+      "useQueryString": "true"
+    });
+
+    Map data = jsonDecode(response.body);
+    List _temp = [];
+
+    for (var i in data['feed']) {
+      _temp.add(i['content']['details']);
+    }
+
+    return Recipe.recipesFromSnapshot(_temp);
+  }
+}
